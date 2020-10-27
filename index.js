123c557f5227488215b7417fb47f59ee4a6e92b4
Nathan Cantat <=>
2020-10-27 11:48:28 +0100

    First POC of CLI

---
+++ index.js
@@ -0,0 +1,40 @@
+const readline = require('readline');
+
+function replDemo() {
+  return new Promise(function(resolve, reject) {
+    let rl = readline.createInterface(process.stdin, process.stdout)
+    rl.setPrompt('elevator> ')
+    rl.prompt();
+    rl.on('line', function(line) {
+      if (line === "exit" || line === "quit" || line == 'q') {
+        rl.close()
+        return // bail here, so rl.prompt() isn't called again
+      }
+
+      if (line === "help" || line === '?') {
+        console.log(`commands:\n  woof\n  exit|quit\n`)
+      } else if (line === "call") {
+        console.log('Elevator coming!')
+      } else {
+        console.log(`unknown command: "${line}"`)
+      }
+      rl.prompt()
+
+    }).on('close',function(){
+      console.log('bye')
+      resolve(42) // this is the final result of the function
+    });
+  })
+}
+
+async function run() {
+  try {
+    let replResult = await replDemo()
+    console.log('repl result:', replResult)
+
+  } catch(e) {
+    console.log('failed:', e)
+  }
+}
+
+run()
